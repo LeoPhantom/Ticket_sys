3d3e7f4baf4fae0cd97dec8ba8527dfb424d5880
LeoPHantom <michael.sheinker23@gmail.com>
2025-12-08 18:30:57 +0200

    added  id to jobs

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for
 import json
 import os
 
+
 app = Flask(__name__)
 
-#tickets = []
+#tickets 
 def load_tickets():
     if os.path.exists("data/tickets.json"):
-        with open("data/tickets.json", "r") as f:
+        with open("data/tickets.json", "r") as f: 
             return json.load(f)
     return [] 
 
@@ -28,6 +29,50 @@ def load_jobs():
 def save_job(jobs):
     with open ("data/jobs.json", "w") as f:
         json.dump(jobs, f , indent=4)
+#index 
+def id_index(type):
+    if type =='job' and os.path.exists("data/job_index.json"):
+        with open ("data/job_index.json") as f:
+            return json.load(f)
+    elif type =='ticket' and os.path.exists("data/ticket_index.json"):
+        with open ("data/ticket_index.json") as f:
+            return json.load(f)
+    else:
+
+        return {"id": 0}   # <-- FIX
+
+def save_id_counter(type, counter):
+    if type == 'job':
+    
+        with open("data/job_index.json", "w") as f:
+            json.dump(counter, f, indent=4)
+
+    elif type == 'ticket':
+
+        with open("data/ticket_index.json", "w") as f:
+            json.dump(counter, f, indent=4)
+
+    else:
+        print("NO such luck")
+
+#ID func
+def set_id(type):
+    if type == 'job':
+        counter = id_index(type)
+        counter["id"] += 1
+        save_id_counter(type, counter)
+        return counter["id"]
+    elif type == 'ticket':
+        counter = id_index(type)
+        counter["id"] += 1
+        save_id_counter(type, counter)
+        return counter["id"]
+    
+
+    
+     
+    
+
 
 @app.route('/')
 def index():
@@ -73,8 +118,11 @@ def new_job():
         end_time = request.form['time_end']
 
         jobs = load_jobs()
+        
+        id = set_id('job')
 
         jobs.append({
+            'id' : id,
             'name': name,
             'operator': operator,
             'description': description,
