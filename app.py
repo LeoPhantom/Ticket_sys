8b8e1e5f51f8092e756691ab85f6f0dce6e33444
LeoPHantom <michael.sheinker23@gmail.com>
2025-12-08 15:56:16 +0200

    Added job page and fucntionality

--- app.py
+++ app.py
@@ -6,16 +6,28 @@ app = Flask(__name__)
 
 #tickets = []
 def load_tickets():
-    if os.path.exists("tickets.json"):
-        with open("tickets.json", "r") as f:
+    if os.path.exists("data/tickets.json"):
+        with open("data/tickets.json", "r") as f:
             return json.load(f)
     return [] 
 
 
 def save_ticket(tickets):
-    with open ("tickets.json", "w") as f:
+    with open ("data/tickets.json", "w") as f:
         json.dump(tickets, f , indent=4)
 
+
+#Job 
+def load_jobs():
+    if os.path.exists("data/jobs.json"):
+        with open("data/jobs.json", "r") as f:
+            return json.load(f)
+    return [] 
+
+
+def save_job(jobs):
+    with open ("data/jobs.json", "w") as f:
+        json.dump(jobs, f , indent=4)
 
 @app.route('/')
 def index():
@@ -47,9 +59,39 @@ def view_tickets():
     tickets = load_tickets()
     return render_template('view_tickets.html', tickets=tickets)
 
-@app.route('/jobs')
-def job_list():
-    return render_template('job_list.html')
+@app.route('/jobs', methods=['GET', 'POST'])
+def new_job():
+    if request.method == 'POST':
+        name = request.form['name']
+        operator = request.form['operator']
+        description = request.form['description']
+        impact = request.form.get('impact', 'no')
+        impact_customer = request.form.get('impactCustomer', 'no')
+        impact_desc = request.form.get('impactDesc', '')
+        start_time = request.form['time_start']
+        end_time = request.form['time_end']
+
+        jobs = load_jobs()
+
+        jobs.append({
+            'name': name,
+            'operator': operator,
+            'description': description,
+            'impact': impact,
+            'impact_customer': impact_customer,
+            'impact_desc': impact_desc,
+            'start_time': start_time,
+            'end_time': end_time
+        })
+
+        save_job(jobs)
+
+        return redirect(url_for('new_job'))  # refresh page after saving
+
+    jobs = load_jobs()
+    return render_template('job_list.html', jobs=jobs)
+
+    
 
 
 if __name__ == "__main__":
