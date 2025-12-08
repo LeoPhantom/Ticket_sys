e4dd938e31c28f945bf356f3e1369578db7bb7cb
LeoPHantom <michael.sheinker23@gmail.com>
2025-12-08 14:05:54 +0200

    App basic funtion
    creat the ticket adn render the ticket , its saving it i memeory no to a
    file .json

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+tickets = []
+
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+@app.route('/new_ticket', methods=['GET', 'POST'])
+def new_ticket():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        severity = request.form['severity']
+
+        tickets.append({
+            'title': title,
+            'description': description,
+            'severity': severity
+        })
+
+        return redirect(url_for('view_tickets'))
+
+    return render_template('new_ticket.html')
+
+@app.route('/tickets')
+def view_tickets():
+    return render_template('view_tickets.html', tickets=tickets)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
